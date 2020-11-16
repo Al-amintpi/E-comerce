fb8839e7847b5e742e81c984a9f96707b1cbfdfc
Al-amintpi <alamincmt7418@gmail.com>
2020-11-17 01:59:44 +0600

    shop design suceess but not responsive

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -181,4 +181,35 @@ $(document).ready(function(){
 		$('.search-box-hidden').toggle();
 		return false;
 	});
+	var acc = document.getElementsByClassName("accordion");
+		console.log(acc.length);
+		var i;
+
+		for (i = 0; i < acc.length; i++) {
+			console.log(i);
+		  acc[i].addEventListener("click", function() {
+		    this.classList.toggle("active");
+		    var panel = this.nextElementSibling;
+		    if (panel.style.maxHeight) {
+		      panel.style.maxHeight = null;
+		    } else {
+		      panel.style.maxHeight = panel.scrollHeight + "px";
+		    } 
+		  });
+		}
 });
+
+$( function() {
+    $( "#slider-range" ).slider({
+      range: true,
+      min: 0,
+      max: 500,
+      values: [ 75, 300 ],
+      slide: function( event, ui ) {
+        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+      }
+    });
+    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+  } );
+
