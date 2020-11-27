97db05706d9c7abf0eddaa05eab8c90f3f18b8c8
Al-amintpi <alamincmt7418@gmail.com>
2020-11-27 10:41:10 +0600

    E-commerce website Complete

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -154,6 +154,23 @@ $(document).ready(function(){
         } 
     }
 	});
+
+// Image popups
+$('.popup').magnificPopup({
+  type: 'image',
+  removalDelay: 500, //delay removal by X to allow out-animation
+  callbacks: {
+    beforeOpen: function() {
+      // just a hack that adds mfp-anim class to markup 
+       this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+       this.st.mainClass = this.st.el.attr('data-effect');
+    }
+  },
+  closeOnContentClick: true,
+  midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+});
+
+
 });
 
 $(window).scroll(function(){
@@ -234,3 +251,5 @@ $(document).ready(function(){
 		  });
 		}
 });
+
+
